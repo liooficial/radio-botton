4c7cc9c8045bcfcbe88d6711af88925a03724b9d
Julio Moreno Castro <jjuli123.@gmail.com>
2023-02-12 14:29:32 -0600

    radioboton

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent j=new Intent(MainActivity2.this,MainActivity.class);
+                startActivity(j);
+            }
+        },4000);
+    }
+}
